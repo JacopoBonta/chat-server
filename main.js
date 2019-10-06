e485dd115f358a3a7257bbfb41849d0823d0d437
JacopoBonta <bonta.jacopo@gmail.com>
2019-10-06 16:51:23 +0200

    add global channel

--- main.js
+++ main.js
@@ -4,9 +4,14 @@ const io = require('socket.io')(http);
 const { SERVER_ADDRESS, SERVER_PORT } = process.env
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+  console.log('user connected!', socket)
+
+  socket.on('global', function(msg){
+    console.log('message: ' + msg);
+    socket.emit(msg)
+  });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(SERVER_PORT, SERVER_ADDRESS, function(){
+  console.log(`listening on ${SERVER_ADDRESS}:${SERVER_PORT}`);
 });
