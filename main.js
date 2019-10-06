ff2d8c1140141c8d3daaa5e8a2038efd51c01b56
JacopoBonta <bonta.jacopo@gmail.com>
2019-10-06 18:48:41 +0200

    broadcast message on global

--- main.js
+++ main.js
@@ -3,12 +3,14 @@ const http = require('http').createServer();
 const io = require('socket.io')(http);
 const { SERVER_ADDRESS, SERVER_PORT } = process.env
 
-io.on('connection', function(socket){
+io.on('connection', function(socket) {
+
+  socket.broadcast.emit('Welcome new user')
 
   socket.on('global', function(msg) {
     console.log('message from: ' + msg.username);
-    console.log('message: ' + msg.text + '\n');
-    socket.emit('global', msg);
+    console.log('message: ' + msg.message + '\n');
+    socket.broadcast.emit('global', msg);
   });
 });
 
