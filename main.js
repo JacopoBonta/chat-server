22e4e53dcb19fc8b15c43214d5a3e573b32fa75c
JacopoBonta <bonta.jacopo@gmail.com>
2019-10-06 16:06:22 +0200

    added server socket.io

---
+++ main.js
@@ -0,0 +1,10 @@
+const http = require('http').createServer();
+const io = require('socket.io')(http);
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
