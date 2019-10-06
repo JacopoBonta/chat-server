59c82e7d1dcd959b2d53fde94a5f84f7586a32c7
JacopoBonta <bonta.jacopo@gmail.com>
2019-10-06 19:11:29 +0200

    track connected user

--- main.js
+++ main.js
@@ -3,17 +3,37 @@ const http = require('http').createServer();
 const io = require('socket.io')(http);
 const { SERVER_ADDRESS, SERVER_PORT } = process.env
 
+// userMap tiene traccia degli utenti che si connettono. La key è l'id del socket ed il value un nome casuale univoco
+const userMap = new Map()
+
 io.on('connection', function(socket) {
 
-  socket.broadcast.emit('Welcome new user')
+  userMap.set(socket.id, `user#${userMap.size}`)
+
+  socket.broadcast.emit('global', {
+    username: 'server',
+    message: 'New user joined to the server!' + userMap.get(socket.id)
+  })
 
   socket.on('global', function(msg) {
     console.log('message from: ' + msg.username);
     console.log('message: ' + msg.message + '\n');
     socket.broadcast.emit('global', msg);
   });
+
+  socket.on('disconnect', function(reason) {
+    const user = userMap.get(socket.id)
+    const msg = `User ${user} diconnect for reason ${reason}`
+    console.log(msg)
+    socket.broadcast.emit('global', {
+      username: 'server',
+      message: msg
+    })
+    userMap.delete(socket.id)
+  })
 });
 
-http.listen(SERVER_PORT, SERVER_ADDRESS, function(){
+
+http.listen(SERVER_PORT, SERVER_ADDRESS, function() {
   console.log(`listening on ${SERVER_ADDRESS}:${SERVER_PORT}`);
 });
